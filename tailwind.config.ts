788a2e018618ec03aebc677271285bcfe38411a1
VoropaievOleksandr <isuszanty@gmail.com>
2024-01-03 03:18:04 +0100

    feat: added flowbite and react-icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,10 +5,11 @@ const config: Config = {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/flowbite-react/lib/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 }
 export default config
