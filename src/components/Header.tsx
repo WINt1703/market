5504805d4114f90f8bfb9f12bd8dfb02f17ce3ba
VoropaievOleksandr <isuszanty@gmail.com>
2024-01-04 02:53:25 +0100

    refactor: replace in Header code to Logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image"
 import Link from "next/link"
 import type { FC } from "react"
 import CartIndicator from "./CartIndicator"
+import Logo from "./Logo"
 import ProductSearch from "./ProductSearch"
-import Typography from "./Typography"
 
 const linkStyle =
   "font-extrabold text-[#274C5B] hover:text-[#5db0d1] active:text-[#036c96]"
@@ -11,12 +10,7 @@ const linkStyle =
 const Header: FC = () => {
   return (
     <div className="flex items-center gap-x-24">
-      <div className="flex items-center justify-center gap-x-1.5">
-        <Image src="/logo.svg" width={30} height={45} alt="logo" />
-        <Typography variant="title" className="text-3xl font-bold">
-          Organick
-        </Typography>
-      </div>
+      <Logo />
       <nav className="space-x-9">
         <Link href="/" className={linkStyle}>
           Home
