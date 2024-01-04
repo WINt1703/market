2cdd96581f9012cc74161c81bfe37e2beced869a
VoropaievOleksandr <isuszanty@gmail.com>
2024-01-04 02:52:33 +0100

    feat: added component Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image"
+import type { FC } from "react"
+import Typography from "./Typography"
+
+const Logo: FC = () => {
+  return (
+    <div className="flex items-center justify-center gap-x-1.5">
+      <Image src="/logo.svg" width={30} height={45} alt="logo" />
+      <Typography variant="title" className="text-3xl font-bold">
+        Organick
+      </Typography>
+    </div>
+  )
+}
+export default Logo
