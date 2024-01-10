1b8bccf6ea8f40e555c184ffb66031e9ddf4f197
VoropaievOleksandr <isuszanty@gmail.com>
2024-01-10 20:47:17 +0100

    fixed: fixed applied styles for Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,23 @@
-import { CustomFlowbiteTheme, Button as FlowbiteButton } from "flowbite-react"
 import { ButtonHTMLAttributes, FC } from "react"
 import { FaArrowRight } from "react-icons/fa6"
+import { twMerge } from "tailwind-merge"
 
 type ButtonProps = {
 	variant?: "primary" | "secondary" | "outline"
 	withoutArrow?: boolean
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
-const customTheme: CustomFlowbiteTheme["button"] = {
-	base: "font-bold !ring-0 hover:opacity-95 py-2 px-3 active:outline outline-2",
+type ButtonTheme = {
+	base: string
+	color: {
+		primary: string
+		secondary: string
+		outline: string
+	}
+}
+
+const theme: ButtonTheme = {
+	base: "flex items-center font-bold ring-0 hover:opacity-95 py-3 px-6 rounded-xl active:outline outline-2",
 	color: {
 		primary: "bg-primary text-white outline-cyan-500",
 		secondary: "bg-secondary text-primary outline-amber-400",
@@ -20,17 +29,24 @@ const Button: FC<ButtonProps> = ({
 	children,
 	withoutArrow,
 	variant,
+	className,
 	...rest
 }) => {
 	return (
-		<FlowbiteButton theme={customTheme} color={variant ?? "primary"} {...rest}>
+		<button
+			className={twMerge(
+				theme.base,
+				variant && theme.color[variant],
+				className
+			)}
+			{...rest}>
 			{children}
 			{!withoutArrow && (
 				<span className="ml-2 grid h-4 w-4 place-content-center rounded-full bg-[#335B6B] text-white ">
 					<FaArrowRight size={10} />
 				</span>
 			)}
-		</FlowbiteButton>
+		</button>
 	)
 }
 
