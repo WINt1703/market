8d40822947012ecc7e0d60f6aaf990912727faed
VoropaievOleksandr <isuszanty@gmail.com>
2024-01-23 01:51:40 +0100

    fixed:  font weight in GallerySection

--- src/app-sections/home/GallerySection.tsx
+++ src/app-sections/home/GallerySection.tsx
@@ -4,7 +4,7 @@ const GallerySection: FC = () => {
 	return (
 		<section className="flex h-[850px] items-center justify-evenly bg-info-100">
 			<div className="grid h-96 w-96 place-content-center bg-organic-juice bg-cover">
-				<p className="w-fit rounded-xl bg-white p-5 text-lg font-bold">
+				<p className="w-fit rounded-xl bg-white p-5 text-lg font-medium">
 					Organic Juice
 				</p>
 			</div>
@@ -14,7 +14,7 @@ const GallerySection: FC = () => {
 				</p>
 			</div>
 			<div className="grid h-96 w-96 place-content-center bg-nuts-cookies bg-cover">
-				<p className="w-fit rounded-xl bg-white p-5 text-lg font-bold">
+				<p className="w-fit rounded-xl bg-white p-5 text-lg font-medium">
 					Nuts Cookies
 				</p>
 			</div>
